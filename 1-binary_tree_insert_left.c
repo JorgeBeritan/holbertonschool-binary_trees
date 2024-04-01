c7215c44c9c8f7f7bfdf19854b66abb82e43631a
JorgeBeritan <inaikmcontacto@gmail.com>
2024-04-01 16:00:47 -0300

    insert node in the left side

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+binary_tree_t
+*binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode;
+
+	newNode = binary_tree_node(parent, value);
+	if (parent->left)
+	{
+		newNode->left = parent->left;
+		parent->left->parent = newNode;
+	}
+
+	parent->left = newNode;
+
+	return (newNode);
+}
