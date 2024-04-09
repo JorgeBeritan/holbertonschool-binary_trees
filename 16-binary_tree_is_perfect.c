36eabf3a5c3b4e0aad0206ba51023b4bb62beef3
JorgeBeritan <inaikmcontacto@gmail.com>
2024-04-09 15:44:03 -0300

    Perfect Tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,13 +1,50 @@
 #include "binary_trees.h"
 
-int
-binary_tree_is_full(const binary_tree_t *tree)
+size_t
+binary_tree_height(const binary_tree_t *tree)
 {
+	if (tree)
+	{
+		int left = 0, right = 0;
+
+		if (tree->right)
+			right = 1 + binary_tree_height(tree->right);
+		if (tree->left)
+			left = 1 + binary_tree_height(tree->left);
+		if (left > right)
+			return (left);
+		else
+			return (right);
+	}
+	else 
+		return (0);
+}
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	unsigned int left, right, n_total = 0;
+
 	if (tree == NULL)
 		return (0);
-	if (tree->left != NULL tree->right != NULL)
-		return (binary_tree_is_full(tree->left) && binary_tree_full(tree->right));
-	else
+
+	left = binary_tree_size(tree->left);
+	right = binary_tree_size(tree->right);
+	n_total = left + right;
+	return (1 + n_total);
+}
+
+int
+binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	unsigned int h = 0, size = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	h = binary_tree_height(tree);
+	size = (1 << (h + 1)) - 1;
+	if (size == binary_tree_size(tree))
 		return (1);
-	return (0);
+	else
+		return (0);
 }
